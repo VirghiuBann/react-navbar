09829efc139a4c3028c2596609e438657b4cada2
virghiu <virghiu@gmail.com>
2023-10-21 14:42:30 -0400

    feat: navbar completed

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,19 +2,32 @@ import { FaBars } from 'react-icons/fa'
 import logo from '../assets/react.svg'
 
 import { links, social } from '../data'
+import { useRef, useState } from 'react'
 
 const NavBar = () => {
+  const [showLinks, setShowLinks] = useState(false)
+  const linksRef = useRef(null)
+
+  const toggleNav = () => {
+    setShowLinks(!showLinks)
+  }
+
+  const linksStyle = {
+    height: showLinks
+      ? `${linksRef.current.getBoundingClientRect().height}px`
+      : '0px',
+  }
   return (
     <nav>
       <div className='nav-container'>
         <div className='nav-header'>
           <img src={logo} alt='logo' />
-          <button className='btn-toggle'>
+          <button className='btn-toggle' onClick={toggleNav}>
             <FaBars />
           </button>
         </div>
-        <div className='links-container'>
-          <ul className='links'>
+        <div className='links-container' style={linksStyle}>
+          <ul className='links' ref={linksRef}>
             {links &&
               links.map(({ id, url, text }) => {
                 return (

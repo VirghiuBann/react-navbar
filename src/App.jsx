9466c514158daa8407e53e52d2e1add43b82b350
virghiu <virghiu@gmail.com>
2023-10-21 13:27:51 -0400

    initial navbar

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import reactLogo from './assets/react.svg'
+
+import NavBar from './components/NavBar'
+
+function App() {
+  return (
+    <main>
+      <NavBar />
+    </main>
+  )
+}
+
+export default App
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+const NavBar = () => {
+  return (
+    <nav>
+      <div className='nav-container'>
+        <div className='nav-header'>logo</div>
+        <div className='links-container'>menu</div>
+        <div className='social-container'>social</div>
+      </div>
+    </nav>
+  )
+}
+
+export default NavBar

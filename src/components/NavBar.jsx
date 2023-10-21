5f6d3582972b9d489097312a7d91197c82986ce1
virghiu <virghiu@gmail.com>
2023-10-21 13:37:17 -0400

    feat: basic structure completed

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,42 @@
+import { FaBars } from 'react-icons/fa'
+import logo from '../assets/react.svg'
+
+import { links, social } from '../data'
+
 const NavBar = () => {
   return (
     <nav>
       <div className='nav-container'>
-        <div className='nav-header'>logo</div>
-        <div className='links-container'>menu</div>
-        <div className='social-container'>social</div>
+        <div className='nav-header'>
+          <img src={logo} alt='logo' />
+          <button>
+            <FaBars />
+          </button>
+        </div>
+        <div className='links-container'>
+          <ul>
+            {links &&
+              links.map(({ id, url, text }) => {
+                return (
+                  <li key={id}>
+                    <a href={url}>{text}</a>
+                  </li>
+                )
+              })}
+          </ul>
+        </div>
+        <div className='social-container'>
+          <ul>
+            {social &&
+              social.map(({ id, url, icon }) => {
+                return (
+                  <li key={id}>
+                    <a href={url}>{icon}</a>
+                  </li>
+                )
+              })}
+          </ul>
+        </div>
       </div>
     </nav>
   )
